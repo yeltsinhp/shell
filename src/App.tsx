f0127f31f5a3f4f569dcc9660009024b98d142af
Yeltsin4493 <yeltsinrhp.93@gmail.com>
2025-02-25 17:26:24 -0500

    add theme v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
 import { useSelector } from "react-redux";
 import { RootState } from "./store";
+import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyles } from "./components/GlobalStyles";
 import Login from "./views/Login";
 
 const App = () => {
-  const [user, setUser] = useState<string | null>(null);
   const theme = useSelector((state: RootState) => state.theme.theme);
 
   return (
     <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
       <GlobalStyles />
-      {user ? <h1>Bienvenido {user}</h1> : <Login onLogin={setUser} />}
+      <Login onLogin={(username) => console.log(username)} />
     </ThemeProvider>
   );
 };
